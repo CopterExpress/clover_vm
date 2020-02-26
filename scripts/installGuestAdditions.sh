ae91bb580c5ebfb88af641a1a42d96361d205aab
Alexey Rogachevskiy <sfalexrog@gmail.com>
2020-02-26 16:17:19 +0300

    scripts: Clean up unneeded scripts

--- scripts/installGuestAdditions.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update
-sudo apt install -y build-essential make perl
-# virtualbox is still terrible, let's target vmware
-sudo apt install -y open-vm-tools
--- scripts/installRosUser.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rosdep update
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
