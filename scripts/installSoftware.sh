5c4b952d350043a3ace08776d689a3e7bb831bd1
Alexey Rogachevskiy <sfalexrog@gmail.com>
2020-03-30 17:01:02 +0300

    scripts: Install rosdep explicitly

--- scripts/installSoftware.sh
+++ scripts/installSoftware.sh
@@ -14,7 +14,7 @@ echo "--- Installing ROS desktop packages"
 
 echo "${PASSWORD}" | sudo -E -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 echo "${PASSWORD}" | sudo -E -S sh -c 'apt-key adv --keyserver "hkp://keyserver.ubuntu.com:80" --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654'
-echo "${PASSWORD}" | sudo -E -S sh -c 'apt update; apt install -y python-rosinstall-generator python-wstool build-essential ros-melodic-desktop'
+echo "${PASSWORD}" | sudo -E -S sh -c 'apt update; apt install -y python-rosdep python-rosinstall-generator python-wstool build-essential ros-melodic-desktop'
 
 echo "${PASSWORD}" | sudo -E -S sh -c 'rosdep init'
 rosdep update
