8709ec587329d758ad609553f1137208172c4ab7
Alexey Rogachevskiy <sfalexrog@gmail.com>
2020-07-05 18:05:59 +0300

    build: Download Packer in CI environment (hack)

--- build.sh
+++ build.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-PACKER=$(which packer)
+# FIXME: Use system Packer if possible
+#PACKER=$(which packer)
 
-if [ "x${PACKER}" == "x" ]; then
+#if [ "x${PACKER}" == "x" ]; then
+PACKER="./packer"
+if [ ! -f ${PACKER} ]; then
     echo "Packer not found; downloading v1.5.4 from Hashicorp"
     wget https://releases.hashicorp.com/packer/1.5.4/packer_1.5.4_linux_amd64.zip
     unzip packer_1.5.4_linux_amd64.zip
     rm packer_1.5.4_linux_amd64.zip
-    PACKER="./packer"
 fi
 
 echo "--- Using Packer version $(${PACKER} --version)"
 
 echo "--- Building base image"
 
-${PACKER} build base_vm.json
+${PACKER} build -only=virtualbox-iso base_vm.json
 
 echo "--- Building extended image"
 
