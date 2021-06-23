763b158fb5e9792aba04b65b20c7da8ec2aa4d75
Oleg Kalachev <okalachev@gmail.com>
2021-06-23 17:45:46 +0300

    Use install_gitbook for gitbook installation

--- scripts/installSoftware.sh
+++ scripts/installSoftware.sh
@@ -117,8 +117,8 @@ export PATH=${HOME}/.npm-global/bin:$PATH
 echo 'export PATH='${HOME}'/.npm-global/bin:$PATH' >> ${HOME}/.bashrc
 echo "--- Installing gitbook and building docs"
 cd ${HOME}/catkin_ws/src/clover
-NPM_CONFIG_UNSAFE_PERM=true npm install gitbook-cli -g
-NPM_CONFIG_UNSAFE_PERM=true gitbook install
+builder/assets/install_gitbook.sh
+gitbook install
 gitbook build
 touch node_modules/CATKIN_IGNORE docs/CATKIN_IGNORE _book/CATKIN_IGNORE clover/www/CATKIN_IGNORE # ignore documentation files by catkin
 
