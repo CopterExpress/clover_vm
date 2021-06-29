d9bd9a531c5d68cea9e8a6c0f18f327560b36c4a
Oleg Kalachev <okalachev@gmail.com>
2021-06-29 03:58:03 +0300

    Build image in GitHub Actions

--- build.sh
+++ build.sh
@@ -9,9 +9,14 @@ set -ex
 PACKER="./packer"
 if [ ! -f ${PACKER} ]; then
     echo "Packer not found; downloading v1.5.4 from Hashicorp"
-    wget --progress=dot:giga https://releases.hashicorp.com/packer/1.5.4/packer_1.5.4_linux_amd64.zip
-    unzip packer_1.5.4_linux_amd64.zip
-    rm packer_1.5.4_linux_amd64.zip
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        OS=linux
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        OS=darwin
+    fi
+    wget --progress=dot:giga https://releases.hashicorp.com/packer/1.5.4/packer_1.5.4_${OS}_amd64.zip
+    unzip packer_1.5.4_${OS}_amd64.zip
+    rm packer_1.5.4_${OS}_amd64.zip
 fi
 
 echo "--- Using Packer version $(${PACKER} --version)"
@@ -28,7 +33,7 @@ ${PACKER} build ros_ide_vm.json
 echo "--- Marking the VM"
 
 GIT_REV=$(git rev-parse --short HEAD)
-GIT_DESCRIBE=$(git describe --tags)
+GIT_DESCRIBE=$(git describe --always)
 VM_NAME="clover-devel_${GIT_DESCRIBE}.ova"
 mv ./output-virtualbox-ovf/clover-devel.ova ./output-virtualbox-ovf/${VM_NAME}
 ls -l output-virtualbox-ovf
