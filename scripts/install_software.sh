ef46ca93bb90cd8f75f1acc692db06adf89550c0
Oleg Kalachev <okalachev@gmail.com>
2022-07-24 02:11:34 +0300

    Attempt

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -88,8 +88,9 @@ make px4_sitl
 echo "--- Building the workspace"
 # sudo -E sh -c 'rm -rf /opt/ros/noetic/include/mavlink'
 ls ~/catkin_ws/src/PX4-Autopilot/build/px4_sitl_default/mavlink
-echo 'target_include_directories(sitl_gazebo PUBLIC "/home/clover/PX4-Autopilot/build/px4_sitl_default/mavlink")' >> ~/catkin_ws/src/clover/clover_simulation/CMakeLists.txt
+echo 'include_directories("/home/clover/PX4-Autopilot/build/px4_sitl_default/mavlink")' >> ~/catkin_ws/src/sitl_gazebo/CMakeLists.txt
 cd ~/catkin_ws
+catkin_make mavlink_c_generate
 catkin_make
 
 echo "--- Installing Visual Studio Code"
