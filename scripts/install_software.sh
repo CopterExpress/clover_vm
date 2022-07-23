0e70776791ad798a2aa476df62cab38ff2c5fadc
Oleg Kalachev <okalachev@gmail.com>
2022-07-23 20:46:06 +0300

    Fix

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -88,7 +88,7 @@ make px4_sitl
 echo "--- Building the workspace"
 # sudo -E sh -c 'rm -rf /opt/ros/noetic/include/mavlink'
 ls ~/catkin_ws/src/PX4-Autopilot/build/px4_sitl_default/mavlink
-echo 'include_directories("/home/clover/PX4-Autopilot/build/px4_sitl_default/mavlink")' >> ~/catkin_ws/src/clover/sitl_gazebo/CMakeLists.txt
+echo 'include_directories("/home/clover/PX4-Autopilot/build/px4_sitl_default/mavlink")' >> ~/catkin_ws/src/sitl_gazebo/CMakeLists.txt
 cd ~/catkin_ws
 catkin_make
 
